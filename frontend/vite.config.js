e36373c6687c781943f3e54efef92f6cf6cd1703
NiloyBlueee <niloynilblue@gmail.com>
2025-07-31 00:47:50 +0600

    fixed configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    }
-  }
+  
 })
