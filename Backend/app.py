16ab32bc11344800353d10d7597e499897d83d3c
NiloyBlueee <niloynilblue@gmail.com>
2025-10-18 21:23:08 +0600

    debug=False 2x RAM

--- Backend/app.py
+++ Backend/app.py
@@ -971,4 +971,4 @@ def send_button_click():
     return jsonify({'status': 'ok', 'telegram': result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
